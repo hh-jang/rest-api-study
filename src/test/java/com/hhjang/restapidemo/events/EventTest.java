7d84326e9ba63df6aa89a7f00d4f4fd5e2f08b1a
hh-jang <jhh1117@daum.net>
2020-10-01 16:40:33 +0900

    Parameter Test 추가
    * junit5에 맞게 테스트 추가

--- src/test/java/com/hhjang/restapidemo/events/EventTest.java
+++ src/test/java/com/hhjang/restapidemo/events/EventTest.java
@@ -1,6 +1,11 @@
 package com.hhjang.restapidemo.events;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,5 +34,52 @@ public class EventTest {
         // Then
         assertThat(event.getName()).isEqualTo(name);
         assertThat(event.getDescription()).isEqualTo(description);
+    }
+
+    @ParameterizedTest(name = "{index} => basePrice={0}, maxPrice={1}, isFree={2}")
+    @MethodSource(value = "parametersForTestFree")
+    public void testFree(int basePrice, int maxPrice, boolean isFree) {
+        // Given
+        Event event = Event.builder()
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
+                .build();
+
+        // When
+        event.update();
+
+        // Then
+        assertThat(event.isFree()).isEqualTo(isFree);
+    }
+
+    @ParameterizedTest(name = "{index} => location={0}, isOffline={1}")
+    @MethodSource(value = "parametersForTestOffline")
+    public void testOffline(String location, boolean isOffline) {
+        // Given
+        Event event = Event.builder()
+                .location(location)
+                .build();
+
+        // When
+        event.update();
+
+        // Then
+        assertThat(event.isOffline()).isEqualTo(isOffline);
+    }
+
+    private static Stream<Arguments> parametersForTestOffline() {
+        return Stream.of(
+                Arguments.of("죽전", true),
+                Arguments.of(null, false),
+                Arguments.of("    ", false)
+        );
+    }
+
+    private static Stream<Arguments> parametersForTestFree() {
+        return Stream.of(
+                Arguments.of(0, 0, true),
+                Arguments.of(100, 0, false),
+                Arguments.of(0, 100, false)
+        );
     }
 }
